312e2161f0d70b32c83588fe70fd5f808419412f
Almond <iwanttobebetter7@gmail.com>
2022-02-23 12:09:06 +0700

    graphic: build libs from source
    in accordance to
    https://github.com/almondnguyen/android_device_samsung_grandppltedx/commit/715c74f1bfacc57fa39216e6739c97c6f6e23520
    
    Signed-off-by: Almond

--- Android.mk
+++ Android.mk
@@ -24,4 +24,24 @@ LOCAL_MULTILIB := 32
 LOCAL_SRC_FILES_32 := proprietary/lib/libdpframework.so
 include $(BUILD_PREBUILT)
 endif
+
+# libgralloc_extra and libion_mtk for 
+# building libui_ext and libgui_ext from-src
+include $(CLEAR_VARS)
+LOCAL_MODULE := libgralloc_extra
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MULTILIB := 32
+LOCAL_SRC_FILES_32 := proprietary/lib/libgralloc_extra.so
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libion_mtk
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MULTILIB := 32
+LOCAL_SRC_FILES_32 := proprietary/lib/libion_mtk.so
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_PREBUILT)
 endif

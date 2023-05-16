700a2fe45a76a10d20c6fcc1d19058ab22a90109
Nguyễn Long <hoanglong.lumi404@gmail.com>
2023-05-16 13:58:24 +0700

    grandppltedx: Move proprietary blobs back to system
    Signed-off-by: Nguyễn Long <hoanglong.lumi404@gmail.com>

--- grandppltedx-vendor-blobs.mk
+++ grandppltedx-vendor-blobs.mk
@@ -1,26 +1,26 @@
 #bin
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/grandppltedx/proprietary/bin/mtk-device-only,$(TARGET_COPY_OUT_VENDOR)/bin)
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/grandppltedx/proprietary/bin/mtk-vendor,$(TARGET_COPY_OUT_VENDOR)/bin)
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/grandppltedx/proprietary/bin/samsung,$(TARGET_COPY_OUT_VENDOR)/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/grandppltedx/proprietary/bin/mtk-device-only,$(TARGET_COPY_OUT_SYSTEM)/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/grandppltedx/proprietary/bin/mtk-vendor,$(TARGET_COPY_OUT_SYSTEM)/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/grandppltedx/proprietary/bin/samsung,$(TARGET_COPY_OUT_SYSTEM)/bin)
 
 # camdata / yuv420
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/grandppltedx/proprietary/cameradata,$(TARGET_COPY_OUT_VENDOR)/cameradata)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/grandppltedx/proprietary/cameradata,$(TARGET_COPY_OUT_SYSTEM)/cameradata)
 
 # self-explanatory
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/grandppltedx/proprietary/etc,$(TARGET_COPY_OUT_VENDOR)/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/grandppltedx/proprietary/etc,$(TARGET_COPY_OUT_SYSTEM)/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/grandppltedx/proprietary/lib,$(TARGET_COPY_OUT_VENDOR)/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/grandppltedx/proprietary/lib,$(TARGET_COPY_OUT_SYSTEM)/lib)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/grandppltedx/proprietary/lib64,$(TARGET_COPY_OUT_VENDOR)/lib64)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/grandppltedx/proprietary/lib64,$(TARGET_COPY_OUT_SYSTEM)/lib64)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/grandppltedx/proprietary/usr,$(TARGET_COPY_OUT_VENDOR)/usr)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/grandppltedx/proprietary/usr,$(TARGET_COPY_OUT_SYSTEM)/usr)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/grandppltedx/proprietary/vendor,$(TARGET_COPY_OUT_VENDOR))
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/grandppltedx/proprietary/tee,$(TARGET_COPY_OUT_VENDOR)/tee)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/grandppltedx/proprietary/tee,$(TARGET_COPY_OUT_SYSTEM)/tee)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/grandppltedx/proprietary/xbin,$(TARGET_COPY_OUT_VENDOR)/xbin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/grandppltedx/proprietary/xbin,$(TARGET_COPY_OUT_SYSTEM)/xbin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/grandppltedx/proprietary/T9DB,$(TARGET_COPY_OUT_VENDOR)/T9DB)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/grandppltedx/proprietary/T9DB,$(TARGET_COPY_OUT_SYSTEM)/T9DB)
 
 

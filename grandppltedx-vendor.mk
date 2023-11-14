2eb2256e98def0003aa2692afa6ef747a7365b1c
Nguyễn Long <hoanglong.lumi404@gmail.com>
2023-11-14 12:53:39 +0700

    grandppltedx: Cleanup blobs
    Signed-off-by: Nguyễn Long <hoanglong.lumi404@gmail.com>

--- grandppltedx-vendor.mk
+++ grandppltedx-vendor.mk
@@ -1,1 +1,11 @@
-$(call inherit-product, vendor/samsung/grandppltedx/grandppltedx-vendor-blobs.mk)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/grandppltedx/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/grandppltedx/proprietary/cameradata,system/cameradata)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/grandppltedx/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/grandppltedx/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/grandppltedx/proprietary/media,system/media)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/grandppltedx/proprietary/vendor,system/vendor)
